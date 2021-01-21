7ccaab593ab065edbb957b3f96da4eac86903033
muhammadheda <mhedanurhidayatulloh@gmail.com>
2021-01-22 00:59:05 +0700

    Menambahkan File View dan Pages

---
+++ app/Views/pages/about.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consectetur ipsam sunt assumenda deserunt doloribus dignissimos consequuntur amet magni, ratione cupiditate voluptate commodi explicabo deleniti dolore, vel maxime, accusamus beatae fugit?</p>
+
+        </div>
+    </div>
+</div>
